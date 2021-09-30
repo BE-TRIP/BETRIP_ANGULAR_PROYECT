be1d2cea9de3c35b516e749a677ba444affd03ac
BLOOOP788 <djfemf123@gmail.com>
2021-09-30 00:49:54 -0500

    añadiendo registro de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 //IMPORTACION DE LOGIN VIAJERO
 import {RegisterTravelerComponent}from '../app/Travelers_/pages/register-traveler/register-traveler.component'
 import { LoginComponent } from './public/login/login.component';
-
+import { TripEventsComponent } from 'BETRIP_ANGULAR_PROYECT/src/app/Travelers_/pages/trip-events/trip-events.component';
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'login',component:LoginComponent},
-  {path:'travelerRegister',component:RegisterTravelerComponent}
-  
+  {path:'travelerRegister',component:RegisterTravelerComponent},
+  {path:'tripEvent',component:TripEventsComponent}
 ];
 
 @NgModule({

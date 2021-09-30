0af45a6acaf847a19877ccb6630eacae39fdd95d
BLOOOP788 <djfemf123@gmail.com>
2021-09-29 23:18:44 -0500

    agregandoarchivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,21 @@ import {MatSortModule}from '@angular/material/sort';
 import {MatCardModule}from '@angular/material/card';
 import { RegisterTravelerComponent } from './Travelers_/pages/register-traveler/register-traveler.component';
 import { TripEventsComponent } from './Travelers_/pages/trip-events/trip-events.component';
-import { LoginComponent } from './public/login/login.component'
+import { LoginComponent } from './public/login/login.component';
+import { KnowMoreComponent } from './Travelers_/pages/know-more/know-more.component';
+import { CreateEventComponent } from './Travelers_/pages/create-event/create-event.component';
+import { SearchEventComponent } from './Travelers_/pages/search-event/search-event.component';
+import { SearchRouteComponent } from './Travelers_/pages/search-route/search-route.component'
 @NgModule({
   declarations: [
     AppComponent,
     RegisterTravelerComponent,
     TripEventsComponent,
-    LoginComponent
+    LoginComponent,
+    KnowMoreComponent,
+    CreateEventComponent,
+    SearchEventComponent,
+    SearchRouteComponent
   ],
   imports: [
     BrowserModule,

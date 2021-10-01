ef1725cb49dab8a5388b8d91632067b35c1c1429
BLOOOP788 <djfemf123@gmail.com>
2021-09-30 19:44:02 -0500

    añadiendo formvalidation

--- src/app/Travelers_/pages/register-traveler/register-traveler.component.ts
+++ src/app/Travelers_/pages/register-traveler/register-traveler.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register-traveler',
   templateUrl: './register-traveler.component.html',
   styleUrls: ['./register-traveler.component.css']
 })
 export class RegisterTravelerComponent implements OnInit {
+  submitted:boolean=false;
+  registerForm: FormGroup=this.formBuilder.group({
+    name:['',{validators:[Validators.required],updateOn:'change'}],
+    age:['',{validators:[Validators.required],updateOn:'change'}],
+    dni:['',{validators:[Validators.required],updateOn:'change'}],
+    email:['',{validators:[Validators.required,Validators.email],updateOn:'change'}],
+    password:['',{validators:[Validators.required,Validators.minLength(8)],updateOn:'change'}],
+    phone:['',{update:"change"}],
+    
+  });
 
-  constructor() { }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.setPhoneValidation();
+    this.setNameValidation(); 
+    this.setAgeValidation();
+    this.setDNIValidation();
   }
+
+  get name(){return this.registerForm.get('name');}
+  get age(){return this.registerForm.get('age');}
+  get dni(){return this.registerForm.get('dni');}
+  get email(){return this.registerForm.get('email');}
+  get password(){return this.registerForm.get('password');}
+  get phone(){return this.registerForm.get('phone');}
+ 
+
+  setPhoneValidation(){
+      const phoneControl=this.registerForm.get('phone');
+      //defaultvalidations
+      phoneControl?.setValidators([Validators.pattern('^[0-9]*$'),Validators.required]);
+
+      //validations based  on Roles
+     
+  };
+  setNameValidation(){
+    const nameControl=this.registerForm.get('name');
+    nameControl?.setValidators([Validators.pattern('[a-zA-Z ]*$'),Validators.required])
+  };
+  setAgeValidation(){
+    const nameControl=this.registerForm.get('age');
+    nameControl?.setValidators([Validators.pattern('^[0-9]*$'),Validators.required])
+  };
+  setDNIValidation(){
+    const nameControl=this.registerForm.get('dni');
+    nameControl?.setValidators([Validators.pattern('^[0-9]*$'),Validators.required])
+  }
+  submitForm(){
+      console.log(this.registerForm.valid);
+      this.submitted=true;
+  }
+  saidSomething(){
+    console.log('registrado exitosamente');
+  }
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSortModule}from '@angular/material/sort';
 import {MatCardModule}from '@angular/material/card';
+import {MatGridListModule} from "@angular/material/grid-list"
 import { RegisterTravelerComponent } from './Travelers_/pages/register-traveler/register-traveler.component';
 import { TripEventsComponent } from './Travelers_/pages/trip-events/trip-events.component';
 import { LoginComponent } from './public/login/login.component';
 import { KnowMoreComponent } from './Travelers_/pages/know-more/know-more.component';
 import { CreateEventComponent } from './Travelers_/pages/create-event/create-event.component';
 import { SearchEventComponent } from './Travelers_/pages/search-event/search-event.component';
-import { SearchRouteComponent } from './Travelers_/pages/search-route/search-route.component'
+import { SearchRouteComponent } from './Travelers_/pages/search-route/search-route.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +52,8 @@ import { SearchRouteComponent } from './Travelers_/pages/search-route/search-rou
     MatFormFieldModule,
     MatIconModule,
     MatSortModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

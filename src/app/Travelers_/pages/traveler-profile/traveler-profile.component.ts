16126f2292fedd663cb4c4dca4d4683151840e43
diegoosorioupc <diegoosorioupc@gmail.com>
2021-11-22 18:00:05 -0500

    Perfil de viajero logeado

--- src/app/Travelers_/pages/traveler-profile/traveler-profile.component.ts
+++ src/app/Travelers_/pages/traveler-profile/traveler-profile.component.ts
@@ -8,21 +8,16 @@ import { TravelersService } from '../../services/travelers.service';
 })
 export class TravelerProfileComponent implements OnInit {
 
-  name= 'Diego Osorio';
-  pfp= "https://image.freepik.com/foto-gratis/primer-plano-hombre-caucasico-impresionado-diciendo-wow-mirando-izquierda-asombrado-revisando-trato-promocion-fondo-blanco-copia-espacio_1258-77230.jpg";
-  age= 12;
-  dni= "82904012";
-  email= "qwoidqoh@as";
-  password= "21212112";
-  phoneNumber= "0991144192";
-  id= 1;
+
 
 
   constructor(private travelersService:TravelersService) { }
   
 
 
-  travelersData:any;
+  logtraveler:any;
+  
+
 
   ngOnInit(): void {
 
@@ -30,11 +25,13 @@ export class TravelerProfileComponent implements OnInit {
   }
 
   getEvents(){
-    this.travelersService.getAll().subscribe(data => {
-      this.travelersData = data;
-      console.log(this.travelersData);
-  
-    });      
+    this.logtraveler= localStorage.getItem('user');
+        console.log(this.logtraveler)
+
+this.logtraveler = JSON.parse(this.logtraveler);
+
+
+              
   }  
 
   
